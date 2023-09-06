2a2099f1572719708f548fa4f8d9960066668503
arun06001 <arun06001@E-PW0234KE>
2023-09-06 12:35:29 +0200

    Add project files.

---
+++ Uppgift 1.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Uppgift1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Du är nästan klar med kapitel 1."); //Skriver: Du är nästan klar med kapitel 1.
+            Console.WriteLine("Du har lärt dig skriva ut tecken som \" och \\."); //Skriver: Du har lärt dig skriva ut tecken som " och \.
+        }
+    }
+}
